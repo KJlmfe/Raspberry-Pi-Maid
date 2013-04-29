04e4b48d58ac19c557af50f687c001474af3d281
kjlmfe <kjlmfe@gmail.com>
2013-04-29 15:53:30 +0800

    Add a new command '发送妹纸图r' 给主人或者主人制定的人

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 include_once( "phpsdk/config.php" );
 include_once( "phpsdk/saetv2.ex.class.php" );
 
+define("DOWNLOAD_CMD", "下载"); 
+define("POST_GIRL_PIC_CMD", "妹纸图"); 
+
 define("MASTER_UID", "1848746107");  //主人新浪微博的uid
+define("MASTER_NAME", "@KJlmfe");  //主人新浪微博的name
+define("MY_NAME", "@KJlmfe的树莓派女仆");
 define("DOWNLOAD_DIR", "/tmp ");  //存储目录
 define("SUCCESS_DOWNLOAD_MSG", "主人，女仆已帮您下载好了");
 define("FAIL_DOWNLOAD_MSG", "主人，您的下载地址有误，小女子无能为力啊");
-define("DOWNLOAD_CMD", "下载"); //每次获取用户微博的最新微博的个数
 define("NUM_OF_WEIBO", 10); //每次获取用户微博的最新微博的个数
+define("DOWNLOAD_LOG_FILE", "log/download.log"); //保存所有已下载过的下载地址
+define("CMD_LOG_FILE", "log/weibo_id.log"); //保存所有已处理过的微博
 
 set_time_limit(0); 
 
@@ -32,7 +38,14 @@ class Maid{
 
         foreach($timeline['statuses'] as $w) {
             $tweet = $w['text']; //获取微博内容
+            if(strpos($tweet, "//") != FALSE) {
+                $tweet = substr($tweet, 0, strpos($tweet, "//")); //对于转发的微博 只获取主人的内容
+            }
             $id = $w['id']; //获取微博ID
+            if(!$this->is_new_cmd($id)){ //避免重复执行主人的命令
+                continue;
+            } 
+
             if(strpos($tweet, DOWNLOAD_CMD)) {
                 preg_match("/http:\/\/t.cn\/[a-z0-9A-Z]*/",$tweet, $matches);
                 $url = $matches[0];
@@ -40,8 +53,25 @@ class Maid{
                     $success = $this->start_download($url);
                     $this->finish_download($id, $success);
                 }
+            } else if(strpos($tweet, POST_GIRL_PIC_CMD)) {
+                $this->post_girl_pic($tweet);
             }
         }
+    }
+
+    /*
+     * 判断id 为$weibo_id 的微博是否被处理过 没有被处理过将自动存到log里
+     * @param $weibo_id 微博id
+     * @return bool
+     */
+    function is_new_cmd($weibo_id) {
+        $contents = file_get_contents(CMD_LOG_FILE);
+        $cmd_lists = explode("\n", $contents);
+        if(array_search($weibo_id, $cmd_lists) === FALSE) {
+            file_put_contents(CMD_LOG_FILE, $weibo_id."\n", FILE_APPEND);
+            return TRUE;
+        }
+        return FALSE;
     }
 
     /*
@@ -85,6 +115,33 @@ class Maid{
             $this->v->send_comment($id, FAIL_DOWNLOAD_MSG);
             $this->v->repost($id, FAIL_DOWNLOAD_MSG);
         }
+    }
+
+    function post_girl_pic($tweet) {
+        $cmd = "wget -O /tmp/tupian.html http://www.tupian.fm/ && grep 'retina=\"http:\/\/.*jpg' /tmp/tupian.html | sed 's/^.*retina=\"//g' | sed 's/\".*$//g' | awk '{a[NR]=$0}END{srand();i=int(rand()*NR+1);print a[i]}'";
+        exec($cmd, $output, $tmp);
+        $img_url = $output[0];
+
+        $all_revicers = "";
+
+        for($i=0; $i<strlen($tweet); $i++) {
+            if($tweet[$i] == '@') {
+                $name = '';
+                while($tweet[$i] != ' ' && $tweet[$i] != ':' &&$i<strlen($tweet)) {
+                    $name .= $tweet[$i++];
+                }
+                if(strpos($all_revicers, $name) === FALSE && strcmp($name, MY_NAME) != 0  && strcmp($name, MASTER_NAME) != 0) {
+                    $all_revicers .= $name." ";
+                }
+            }
+        }
+
+        if($all_revicers == "") {
+            $status = "请主人".MASTER_NAME." 验图";
+        } else {
+            $status = "请主人".MASTER_NAME." 的朋友 ".$all_revicers." 验图";
+        }
+        $this->v->upload($status, $img_url);
     }
 
 }
